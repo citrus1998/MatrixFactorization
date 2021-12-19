63a60df30c682f067c7f3cb5a1b29c27064b25f6
citrus1998 <e535061u@gmail.com>
2021-12-19 01:37:34 +0000

    Dec 19 2021

--- scripts/jester_nhp.sh
+++ scripts/jester_nhp.sh
@@ -5,5 +5,5 @@ val=$(cat scripts/log.txt)
 #https://qiita.com/Kobecow/items/bd2cee4b5f2ab51f656b
 nohup python3 jester.py \
 --num_epochs 100 \
---plot "$val_$today" \
+--plot "jester/$val_$today" \
 > log/jester/$val_$today.text & \
--- scripts/movie_nhp.sh
+++ scripts/movie_nhp.sh
@@ -4,6 +4,6 @@ val=$(cat scripts/log.txt)
 
 #https://qiita.com/Kobecow/items/bd2cee4b5f2ab51f656b
 nohup python3 main.py \
---num_epochs 3 \
---plot "$val_$today" \
+--num_epochs 100 \
+--plot "movielens/$val_$today" \
 > log/movielens/$val_$today.text & \
